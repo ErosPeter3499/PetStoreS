f4f08e62e23ecccf4d3f5ca150993201d3e31d09
ErosPeter3499 <eros.peter.1974@gmail.com>
2024-06-02 16:57:38 +0200

    First branch
    Created for the first modification, logout, login endpoint

--- src/test/java/com/example/sandbox/getPet/petDetailTest.java
+++ src/test/java/com/example/sandbox/getPet/petDetailTest.java
@@ -1,28 +1,61 @@
 package com.example.sandbox.getPet;
 
-import com.example.sandbox.Common;
-import io.restassured.response.Response;
-import org.testng.Assert;
-import org.testng.annotations.Test;
-
 import java.util.Map;
 import java.util.TreeMap;
 
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import com.example.sandbox.Common;
 import static com.example.sandbox.util.constans.Tags.SMOKE;
+
+import io.restassured.response.Response;
 
 public class petDetailTest extends Common {
 
-    @Test(enabled = true,groups = {SMOKE},description ="description")
+    /*@Test(enabled = true,groups = {SMOKE},description ="description")
     public void Test1(){
         Map<String, String> queryParams = new TreeMap<>();
-        queryParams.put("status","available");
+        queryParams.put("status","sold");
 
         Response  response = getUrl(findByStatus, queryParams);
-        Assert.assertEquals(response.getStatusCode(),200,"Invalid response code");
+        Assert.assertEquals(response.getStatusCode(),200,"0Invalid response code");
 
         String id = response.jsonPath().get("[0].id").toString();
 
         Response  response2 = getUrl(petById+id);
         Assert.assertEquals(response2.getStatusCode(),200,"Invalid response code");
+    }*/
+
+    @Test(enabled = true,groups = {SMOKE},description ="For a user the logout is successful.")
+    public void TestLoginInValidUserUnSuccessful(){
+        Response  response = getUrl(logout);
+        Integer statusCode = response.getStatusCode();
+        Assert.assertEquals(statusCode,200);
+        System.err.println("StatusCode:" + statusCode.toString());
+
+
+//        String id = response.jsonPath().get("[0].id").toString();
+
+        /*Response  response2 = getUrl(petById+id);
+        Assert.assertEquals(response2.getStatusCode(),200,"Invalid response code");*/
+    }
+    
+    @Test(enabled = true,groups = {SMOKE},description ="For a valid user the given password is not correct.")
+    public void TestLoginValidUserUnSuccessful(){
+        Map<String, String> queryParams = new TreeMap<>();
+        queryParams.put("username","epteers");
+        queryParams.put("password", "epteers0");
+
+        Response  response = getUrl(login,queryParams);
+        Integer statusCode = response.getStatusCode();
+        Assert.assertEquals(statusCode,401);
+        System.err.println("StatusCode:" + statusCode.toString());
+
+
+//        String id = response.jsonPath().get("[0].id").toString();
+
+        /*Response  response2 = getUrl(petById+id);
+        Assert.assertEquals(response2.getStatusCode(),200,"Invalid response code");*/
     }
 }
